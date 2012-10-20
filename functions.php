4b2f1fc96d64bac93c0e11f5e73044188fb103cd
Jonathan Dean <jon@jonathandean.com>
2012-10-20 15:32:50 -0400

    tabs to spaces

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function register_menu() {
-	register_nav_menu('primary-menu', __('Primary Menu'));
+  register_nav_menu('primary-menu', __('Primary Menu'));
 }
 add_action('init', 'register_menu');
 ?>
