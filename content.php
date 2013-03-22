bb25bf494d2418550853c19eb5df77a0c586c8fb
Jonathan Dean <jon@jonathandean.com>
2013-03-21 21:35:53 -0400

    Don't show header or footer meta or comments in single pages

--- content.php
+++ content.php
@@ -13,7 +13,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class($article_class_extra); ?>>
   <header class="article">
     <h1><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-    <p class="date"><span>on</span> <time datetime="<?php the_time('c'); ?>"><?php the_time( get_option('date_format') ); ?></time></p>
+    <?php if(!is_page()) : ?>
+      <p class="date"><span>on</span> <time datetime="<?php the_time('c'); ?>"><?php the_time( get_option('date_format') ); ?></time></p>
+    <?php endif; // !is_page() ?>
   </header>
   <?php if ( is_search() ) : // Only display Excerpts for Search ?>
     <div class="summary">
@@ -23,22 +25,24 @@
     <div class="content">
       <?php the_content('continue reading...'); ?>
     </div>
-  <?php endif; ?>
-  <footer class="meta">
-    <cite class="author"><span>by</span> <?php the_author(); ?></cite>
-    <time datetime="<?php the_time('c'); ?>" class="date"><span>on</span> <?php the_time( get_option('date_format') ); ?></time>
-    <span class="category"><span>in</span> <?php the_category(', ') ?></span>
-    <span class="comments"><span>with</span>  <?php comments_popup_link(__('No comments', 'framework'), __('1 Comment', 'framework'), __('% Comments', 'framework')); ?></span>
-    <?php edit_post_link( __('edit', 'framework'), '<span class="edit-post">[', ']</span>' ); ?>
-    <?php /* <a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php bloginfo('name'); ?>: <?php the_title(); ?>," data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script> */ ?>
-  </footer>
-  <section class="comments">
-    <article class="comments">
-    <?php
-      // If comments are open or we have at least one comment, load up the comment template
-      if ( comments_open() || '0' != get_comments_number() )
-        comments_template( '', true );
-      ?>
-    </article>
-  </section>
+  <?php endif; // is_search() ?>
+  <?php if(!is_page()) : ?>
+    <footer class="meta">
+      <cite class="author"><span>by</span> <?php the_author(); ?></cite>
+      <time datetime="<?php the_time('c'); ?>" class="date"><span>on</span> <?php the_time( get_option('date_format') ); ?></time>
+      <span class="category"><span>in</span> <?php the_category(', ') ?></span>
+      <span class="comments"><span>with</span>  <?php comments_popup_link(__('No comments', 'framework'), __('1 Comment', 'framework'), __('% Comments', 'framework')); ?></span>
+      <?php edit_post_link( __('edit', 'framework'), '<span class="edit-post">[', ']</span>' ); ?>
+      <?php /* <a href="http://twitter.com/share" class="twitter-share-button" data-url="<?php the_permalink(); ?>" data-text="<?php bloginfo('name'); ?>: <?php the_title(); ?>," data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script> */ ?>
+    </footer>
+    <section class="comments">
+      <article class="comments">
+      <?php
+        // If comments are open or we have at least one comment, load up the comment template
+        if ( comments_open() || '0' != get_comments_number() )
+          comments_template( '', true );
+        ?>
+      </article>
+    </section>
+  <?php endif; // !is_page() ?>
 </article>
