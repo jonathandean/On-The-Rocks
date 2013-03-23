ba381443e7a936af82adf36e9279e18dd323bff5
Jonathan Dean <jon@jonathandean.com>
2013-03-23 17:02:40 -0400

    Add a basic footer with copyright info

--- footer.php
+++ footer.php
@@ -6,6 +6,10 @@
 
   wp_footer();
 ?>
+  <footer class="main">
+    <!-- You may remove the credits to me but I do greatly appreciate it if you keep them :) Thanks! ~Jonathan Dean -->
+    <p>&copy; <?php echo date( 'Y' ); ?> <?php bloginfo('name'); ?>. Powered by <a href="http://wordpress.org">WordPress</a> and the <a href="https://github.com/jonathandean/On-The-Rocks">On The Rocks</a> theme by <a href="http://jonathandean.com">Jonathan Dean</a>.</p>
+  </footer>
   </div><?php /* #page */ ?>
 </body>
 </html>
